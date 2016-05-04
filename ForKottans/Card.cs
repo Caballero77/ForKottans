9ed6ea5c68c268d801715fd8b9ead2da18d59d0c
Андрій <caballiero@gmail.com>
2016-05-04 12:25:30 +0300

    First commit

---
+++ ForKottans/Card.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ForKottans
+{
+    class Card
+    {
+        public static Dictionary<string, Regex> Vendors;
+
+        public static string GetCredicCardVendor(string number)
+        {
+            InitializeVendors();
+
+            while(number.Contains(' '))
+                number = number.Remove(number.IndexOf(' '),1);
+
+            foreach (KeyValuePair<string, Regex> t in Vendors)
+                if(t.Value.IsMatch(number))
+                    return t.Key;
+
+            return "Unknown";
+        }
+
+        public static bool IsCreditCardNumerValid(string number)
+        {
+            InitializeVendors();
+
+            bool buf = false;
+            
+            foreach(KeyValuePair<string,Regex> p in Vendors)
+            {
+                buf |= p.Value.IsMatch(number);
+            }
+
+            return (GetIdentificationNumber(number) == 0) && buf;
+        }
+
+        public static string GenerateNextCredicCardNumber(string number)
+        {
+            InitializeVendors();
+
+            while (number.Contains(' '))
+                number = number.Remove(number.IndexOf(' '), 1);
+
+            ulong numb = Convert.ToUInt64(number);
+
+            string buf;
+
+            do
+            {
+                numb = numb - (numb % 10) + 10;
+                buf = Convert.ToString((10 - (ulong)GetIdentificationNumber(Convert.ToString(numb))) % 10 + numb);
+            } while (!IsCreditCardNumerValid(buf));
+
+            return buf;
+        }
+
+        private static void InitializeVendors()
+        {
+            if (Vendors == null)
+            {
+                Vendors = new Dictionary<string, Regex>();
+                Vendors.Add("American Expres", new Regex(@"^3[4,7][0-9]{13}$"));
+                Vendors.Add("Visa", new Regex(@"^4([0-9]{12}|[0-9]{15}|[0-9]{18})$"));
+                Vendors.Add("MasterCard", new Regex(@"^5[1-5]{1}[0-9]{14}$"));
+                Vendors.Add("JCB", new Regex(@"^(35(2[8-9]|[3-8][0-9]))[0-9]{12}$"));
+                Vendors.Add("Maestro", new Regex(@"^(5[0,6-9]|6[0-9])([0-9]){10,17}$"));
+            }
+        }
+
+        private static int GetIdentificationNumber(string number)
+        {
+            while (number.Contains(' '))
+                number = number.Remove(number.IndexOf(' '), 1);
+
+            int[] aray = number.Reverse().ToArray().Select(arg => int.Parse(new string(new char[] { arg }))).ToArray();
+
+            int res = 0;
+
+            for (int i = 0; i < aray.Length; i++)
+                if (i % 2 == 1)
+                    if (aray[i] * 2 < 9)
+                        res += aray[i] * 2;
+                    else
+                        res += aray[i] * 2 - 9;
+                else res += aray[i];
+            return res%10;
+        }
+
+    }
+}
---
+++ ForKottans/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForKottans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string number = "5457082236147000";
+            bool res = true;
+
+            for (int i = 0; i < 2000000; i++)
+            {
+                number = Card.GenerateNextCredicCardNumber(number);
+                res &= Card.IsCreditCardNumerValid(number);
+                //Console.WriteLine(number);
+            }
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+
+            return;
+
+
+            Console.WriteLine(Card.IsCreditCardNumerValid("5457082236147000"));
+
+
+            Console.ReadLine();
+            /*
+            American Expres - 34****,37**** 15
+            Visa 4***** 13,16,19
+            MasterCard 51****-55****(,2221**-2720****) 16
+            Maestro 50****,56****- 69**** 12-19
+            JCB 3528** - 3589** 16
+            */
+            Console.WindowHeight += 30;
+
+            //Maestro
+            Console.WriteLine("Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("5005678438955434565") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("61674975867456") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("565678438904") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("6956784389065457") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("6590 5737 5465") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("6309 8450 2454 2355") == "Maestro");
+            Console.WriteLine(Card.GetCredicCardVendor("5009 5466 3457 543") == "Maestro");
+
+            //JCB 
+            Console.WriteLine("\n\nJBC");
+            Console.WriteLine(Card.GetCredicCardVendor("3580 4534 6545 5434") == "JCB");
+            Console.WriteLine(Card.GetCredicCardVendor("3570 4534 6545 5434") == "JCB");
+            Console.WriteLine(Card.GetCredicCardVendor("3560 4534 6545 5434") == "JCB");
+            Console.WriteLine(Card.GetCredicCardVendor("3589 4534 6545 5434") == "JCB");
+            Console.WriteLine(Card.GetCredicCardVendor("4534 3528 6545 5434") != "JCB");
+
+
+            //MasterCard
+            Console.WriteLine("\n\nMasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("558 956 2343 546783") == "MasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("518956 234 3546783") == "MasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("5289 562343546783") == "MasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("53895623435467 83") == "MasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("54895623435467 8 3") == "MasterCard");
+            Console.WriteLine(Card.GetCredicCardVendor("5289 5623 4354 6783") == "MasterCard");
+
+            // Visa
+            Console.WriteLine("\n\nVisa");
+            Console.WriteLine(Card.GetCredicCardVendor("45 65 67 78 65 23 7") == "Visa");
+            Console.WriteLine(Card.GetCredicCardVendor("4 465674673456789") == "Visa");
+            Console.WriteLine(Card.GetCredicCardVendor("4534 6787 3452 7867") == "Visa");
+            Console.WriteLine(Card.GetCredicCardVendor("453 564 567 234 546 785 5") == "Visa");
+            Console.WriteLine(Card.GetCredicCardVendor("5658964563243") != "Visa");
+            Console.WriteLine(Card.GetCredicCardVendor("4444444444444") == "Visa");
+
+            //American Expres
+            Console.WriteLine("\n\nAmerican Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("373454323432344") == "American Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("349090444444444") == "American Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("3789 0967 8767 987") == "American Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("343434343434343") == "American Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("37 23 54 12 34 54 32 4") == "American Expres");
+            Console.WriteLine(Card.GetCredicCardVendor("34564 23432 57654") == "American Expres");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
